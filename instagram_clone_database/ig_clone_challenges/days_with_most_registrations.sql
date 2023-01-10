7d1b8544a6db7e9ede4c9bed67198b397b55d8fa
IvanTsvetanov <ivan.iv.cv@gmail.com>
2023-01-10 14:46:59 +0200

    rename folder

--- instagram_clone_database/ig_clone_challenges/days_with_most_registrations.sql
+++
@@ -1,9 +0,0 @@
--- 2. What 3 days of the week do most users register on?
-
-SELECT 
-	DAYNAME(created_at) AS day_of_week,
-    COUNT(*) AS total_registrations,
-    RANK() OVER(ORDER BY COUNT(*) DESC) AS ranking
-FROM users
-GROUP BY day_of_week
-LIMIT 3;
--- instagram_clone_database/ig_clone_challenges/finding_oldest_users.sql
+++
@@ -1,6 +0,0 @@
--- 1. Finding 5 oldest users to reward them
-
-SELECT * 
-FROM users
-ORDER BY created_at 
-LIMIT 5;
